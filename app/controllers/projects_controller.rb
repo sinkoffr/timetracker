de04ee665f35b96c7e7e8c0a34322af77df7453a
Rebecca <rebecca.sinkoff@gmail.com>
2018-10-11 19:00:59 +0000

    Added new project modal and devise gem

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,21 @@
 class ProjectsController < ApplicationController
-    def index
-        @projects = Project.all
-    end
+  def index
+    @projects = Project.all
+    @project = Project.new
+  end
+  
+  def create
+    Project.create(project_params)
+    redirect_to root_path
+  end
+  
+  def new
+      
+  end
+  
+  private
+  
+  def project_params
+    params.require(:project).permit(:name, :description) 
+  end
 end
